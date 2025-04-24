fc2c9a595af4206c1d054d2658971dbca75cc843
h1nttt61 <Lyl1.32@yandex.ru>
2025-04-24 21:00:36 +0300

    Work with 3 constructor

--- Classes/Classes.cpp
+++ Classes/Classes.cpp
@@ -12,28 +12,37 @@ class Person
 		{
 			std::cout << "Name: " << name << "\tage: " << age << std::endl;
 		}	
+		Person(std::string p_name, unsigned p_age)
+		{
+			name = p_name;
+			age = p_age;
+			std::cout << "Constuctor created" << std::endl;
+		}
+		Person(std::string p_name) : Person(p_name, 18) // вызов первого конструктора
+		{
+			std::cout << "Second constructor" << std::endl;
+		}
+		Person() : Person(std::string("Undefined")) // вызов второго конструктора
+		{
+			std::cout << "Third constructor" << std::endl;
+		}
 };
 
 int main()
 {
-	/*Person person;
+	/*Person person; //если не писать конструктор , то будет конструктор по умолчанию
 	person.name = "Vitaly";
 	person.age = 18;
 	person.print();*/
-	Person person;
+	/*Person person; //С конструктором по умолчанию, но указатель
 	Person* prt = &person;
 	prt->age = 19;
 	prt->name = "Vitaly";
-	prt->print();
+	prt->print();*/
+	Person person("Vitaly", 19);
+	person.print();
+	Person tom("Tom");
+	tom.print();	
+	Person leha;
+	leha.print();
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
