714df07b6069914bcb304f845352d846fb6c68e1
h1nttt61 <Lyl1.32@yandex.ru>
2025-05-04 20:47:42 +0300

    class book

--- Classes/Classes12_Dynamic_Cast.cpp
+++ Classes/Classes12_Dynamic_Cast.cpp
@@ -1,5 +1,20 @@
 #include "iostream"
 
+class Book
+{
+public:
+	Book(std::string title, unsigned pages) : title{ title }, pages{ pages } {}
+	std::string getTitle() const { return title; }
+	unsigned getPages() const { return pages; }
+	virtual void print() const
+	{
+		std::cout << title << " pages: " << pages << std::endl;
+	}
+private:
+	std::string title;
+	unsigned pages;
+};
+
 int main()
 {
 
