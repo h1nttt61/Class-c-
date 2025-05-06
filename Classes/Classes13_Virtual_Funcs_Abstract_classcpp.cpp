114a402107b72247de6781b5818584881c0fed36
h1nttt61 <Lyl1.32@yandex.ru>
2025-05-06 12:22:11 +0300

    Added fully worked class

--- Classes/Classes13_Virtual_Funcs_Abstract_classcpp.cpp
+++ Classes/Classes13_Virtual_Funcs_Abstract_classcpp.cpp
@@ -14,18 +14,44 @@
 /// <summary>
 /// Abstract class
 /// </summary>
-class SHape
+class Shape
 {
 public:
 	virtual double getSquare() const = 0;
-	virtual double getPerimater() const = 0;
+	virtual double getPerimeter() const = 0;
 };
 
+class Renctagle : public Shape
+{
+public:
+	Renctagle(double width, double height) : width{ width }, height{ height } {}
+	double getSquare() const override { return width * height; }
+	double getPerimeter() const override { return (width + height) * 2; }
+private:
+	double width;
+	double height;
+};
 
+class Circle : public Shape
+{
+public:
+	Circle(double r) : radius{ r } {}
+	double getSquare() const override { return 3.14 * radius * radius; }
+	double getPerimeter() const override { return 2 * 3.14 * radius; }
+private:
+	double radius;
+};
 
 //using namespace std;
 void main()
 {
+	Renctagle rect{ 30, 50 };
+	Circle circle{30};
+
+	std::cout << "Rectangle square: " << rect.getSquare() << std::endl;
+	std::cout << "Rectangle perimeter: " << rect.getPerimeter() << std::endl;
+	std::cout << "Circle square: " << circle.getSquare() << std::endl;
+	std::cout << "Circle perimeter: " << circle.getPerimeter() << std::endl;
 	/*
 	//const char* b = "213";
 	//int a = atoi(b); std::stoi
