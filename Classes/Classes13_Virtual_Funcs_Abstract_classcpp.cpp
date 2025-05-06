ba39803203ef310d142365d6f9a2e45e7281c618
h1nttt61 <Lyl1.32@yandex.ru>
2025-05-06 12:11:46 +0300

    Added abstract class

---
+++ Classes/Classes13_Virtual_Funcs_Abstract_classcpp.cpp
@@ -0,0 +1,27 @@
+#include "iostream"
+#include "sstream"
+#include "string"
+/// <summary>
+/// Abstract class
+/// </summary>
+class SHape
+{
+public:
+	virtual double getSquare() const = 0;
+	virtual double getPerimater() const = 0;
+};
+
+
+
+//using namespace std;
+void main()
+{
+	/*
+	//const char* b = "213";
+	//int a = atoi(b); std::stoi
+	std::string b = "213";
+	int a;
+	std::stringstream(b) >> a;
+	std::cout << a << std::endl;
+	*/
+}
